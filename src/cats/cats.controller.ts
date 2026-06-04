ca3b003650f6f73fa48bfa741fd1c20551c16ed6
Licona13 <kevinliconasilva@gmail.com>
2026-06-04 09:40:11 -0600

    feature/cats: agregar endpoint get /cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './entities/cat.entity';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+
+    constructor(private catsService: CatsService) { }
+
+    @Get()
+    findAll(): Cat[] {
+        return this.catsService.findAll();
+    }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+
+    private cats: Cat[] = [
+        { id: 1, name: 'Boris', age: 3, weight: 4, color: 'blanco' },
+        { id: 2, name: 'Cristobal', age: 5, weight: 3.5, color: 'negro' },
+        { id: 3, name: 'Bicho', age: 1, weight: 2, color: 'naranja' },
+    ];
+
+    findAll(): Cat[] {
+        return this.cats;
+    }
+}
